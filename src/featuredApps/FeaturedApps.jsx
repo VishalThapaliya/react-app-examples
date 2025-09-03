4720c8112350fb6e25cd1ceb626868484dd10d83
Bishal Thapaliya <bishalthapaliya@Bishals-MacBook-Pro.local>
2025-09-03 16:21:03 +0200

    fix typo to go the applications link

--- src/featuredApps/FeaturedApps.jsx
+++ src/featuredApps/FeaturedApps.jsx
@@ -89,7 +89,7 @@ const FeaturedApps = () => {
             </div>
 
             <div className="section-footer">
-                <Link to='/applicatons' className='btn-view-all'>
+                <Link to='/applications' className='btn-view-all'>
                     View All Applications
                     <svg width="20" height="20" viewBox="0 0 20 20" fill="currentColor">
                         <path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd" />
